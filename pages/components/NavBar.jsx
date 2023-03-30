302de480b821ffed3d685ff999262b62bf85da43
Nathan Cyr <nathan.cyr@gmail.com>
2023-03-30 17:01:41 -0400

    correct push

--- pages/components/NavBar.jsx
+++ pages/components/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
     const handleNav = () =>{
         setNav(true);
     }
-
+    
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
